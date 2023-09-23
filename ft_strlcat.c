0ee82dd542003dc5c86571088cf842170f5530a7
Kriwn <nonisgod@gmail.com>
2023-09-23 17:03:26 +0700

    fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -18,35 +18,31 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	j;
 
 	j = 0;
-	i = 0;
-	if(!dst && !src)
+	i = ft_strlen(dst);
 	if (size <= ft_strlen(dst))
 		return (ft_strlen(src) + size);
-	while(dst[i])
-		i++;
-	while(src[j] && j < size)
+	while(src[j] && i + j + 1 < size)
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	return (ft_strlen(dst) + ft_strlen(src));
+	return (i + ft_strlen(src));
 }
 
-/*
-int main()
-{
-	char dest[30];
-	memset(dest, 0, 30);
-	char test[30];
-	memset(test, 0, 30);
-	//char * src = (char *)"AAAAAAAAA";
-	dest[0] = 'B';
-	test[0] = 'B';
+//#include <bsd/string.h>
 
-	printf("%s\n",dest);
-	printf("%ld\n",ft_strlcat(dest,"123",1));
-	printf("%ld\n",strlcat(dest,"123",1));
-	printf("%s\n",dest);
-}
-*/
+//int main()
+//{
+//	char dest[30];
+//	char test[30];
+//	char * src = (char *)"AAAAAAAAA";
+//	memset(dest, 'B', 4);
+//	memset(test, 'B', 4);
+
+//	printf("%ld\n",ft_strlcat(dest,src,6));
+//	printf("%s\n",dest);
+//	printf("%ld\n",strlcat(test,src,6));
+//	printf("%s\n",test);
+//}
+
